6864aa40c041edbdfccb1bbc458be40477279261
Cefe_a8xp@itstep.edu.az <Cefe_a8xp@itstep.edu.az>
2021-05-20 13:34:37 +0400

    .

--- MVP Patterns Practice/Models/Payment.cs
+++ MVP Patterns Practice/Models/Payment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace MVP_Patterns_Practice.Models
 {
     public class Payment
     {
+        [Key]
         public string Name { get; set; }
         public string Liter { get; set; }
         public string Price { get; set; }
